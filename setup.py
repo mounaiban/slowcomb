912f43554e7ce7776c171fdda2bb66b48e4f75ba
Mounaiban <29518031+mounaiban@users.noreply.github.com>
2019-03-19 19:27:09 +1100

    Add setuptools packaging script

---
+++ setup.py
@@ -0,0 +1,46 @@
+"""
+setup-github — packaging script for slowcomb cloned from GitHub
+"""
+
+# Classifiers taken from the Python Software Foundation. Classifiers.
+#  https://pypi.org/classifiers/
+
+import os
+import datetime
+from setuptools import setup, find_packages
+
+# Vital Info
+#
+PACKING_TIME_UTC = datetime.datetime.utcnow().isoformat()
+
+# README: Dump the contents of README.rst
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README=readme.read()
+
+VERSION = "0.7.dev.{0}".format(PACKING_TIME_UTC)
+
+# Invocation of setup()
+#
+setup(
+    name="slowcomb",
+    version=VERSION,
+    packages=find_packages(),
+    author="Mounaiban",
+    author_email="whatever@mounaiban.com",
+    description="""General-purpose combinatorics library with
+    addressable results""",
+    keywords="combinatorics combination permutation mathematics library",
+    long_description=README,
+    project_urls={
+        "Source Code" : "https://github.com/mounaiban/slowcomb",
+        "Bug Tracker" : "https://github.com/mounaiban/slowcomb/issues",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+)
+

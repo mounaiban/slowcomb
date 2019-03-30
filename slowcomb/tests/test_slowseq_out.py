f51e1851d74cf5c9753d326b103536df6448baf1
Mounaiban <29518031+mounaiban@users.noreply.github.com>
2019-03-31 01:43:36 +1100

    Move TestNumberSequenceFactory to shared test specimens
    The method definition has been moved to slowcomb/tests/examples.py

--- slowcomb/tests/test_slowseq_out.py
+++ slowcomb/tests/test_slowseq_out.py
@@ -21,65 +21,19 @@ test_slowseq_out — Output Tests for supporting sequence classes
 #
 
 import unittest
+from slowcomb.tests.examples import OrderOfMagnitudeSequenceFactory
 from slowcomb.slowseq import *
-
-class TestNumberSequenceFactory:
-    """Helper class to prepare standard test items for sequence
-    output tests.
-    """
-
-    # Class Variables
-    #
-    func_n_ord_mag = lambda self,n: n * 10**n
-        # Returns n raised (for positive n) or lowered 
-        # (for negative n) by n orders of magnitude.
-        # i.e. 0, 10, 200, 3000, 40000, 500000 ... , and
-        #  -0.1, -0.02, -0.003, -0.0004 ...
-        # A safe and sane limit of 12 is recommended.
-        #
-        # TODO: Why do I need to have a self argument when I define
-        #  a lambda at class level, but not when it is inside a method?
-    pos_ii_start = 5
-        # All positive ii_start sequences start at internal index 5
-    neg_ii_start = -10
-        # All negative ii_start sequences start at internal index -10
-    neg_ii_start_zero_x = -5
-        # All zero-crossing negative ii_start sequences start at 
-        # internal index -10
-    test_len = 10
-        # All test items are 10 terms long, including the zero'th item
-        # where applicable
-    
-    def get_test_seq_pos_ii(self):
-        return self._seq_class(self.func_n_ord_mag,
-            ii_start=self.pos_ii_start, length=self.test_len)
-
-    def get_test_seq_neg_ii(self):
-        return self._seq_class(self.func_n_ord_mag,
-            ii_start=self.neg_ii_start, length=self.test_len)
-
-    def get_test_seq_neg_ii_zero_x(self):
-        return self._seq_class(self.func_n_ord_mag,
-            ii_start=neg_ii_start_zero_x, length=test_len)
-
-    def get_test_seq_zero_ii(self):
-        return self._seq_class(self.func_n_ord_mag,
-            ii_start=0, length=self.test_len)
-
-    def __init__(self, seq_class):
-        if issubclass(seq_class, NumberSequence) is True:
-            self._seq_class = seq_class
 
 # Slice Indexing Output Tests
 #
 class NumberSequenceSliceOutputTests(unittest.TestCase):
-
     def setUp(self):
-        self.item_factory=TestNumberSequenceFactory(NumberSequence)
+        self.item_factory=OrderOfMagnitudeSequenceFactory(
+            NumberSequence)
         self.test_item_pos_ii=self.item_factory.get_test_seq_pos_ii()
         self.test_item_neg_ii=self.item_factory.get_test_seq_neg_ii()
         self.test_item_zero_ii=self.item_factory.get_test_seq_zero_ii()
-        # Please see the TestNumberSequenceFactory for test
+        # Please see the OrderOfMagnitudeSequenceFactory for test
         # standards used throughout these tests.
         # NumberSequences return tuples for multiple item lookups.
 
@@ -150,7 +104,8 @@ class CacheableSequenceSOTests(NumberSequenceSliceOutputTests):
     TestClass=CacheableSequence
 
     def setUp(self):
-        self.item_factory=TestNumberSequenceFactory(self.TestClass)
+        self.item_factory=OrderOfMagnitudeSequenceFactory(
+            self.TestClass)
         self.test_item_pos_ii=self.item_factory.get_test_seq_pos_ii()
         self.test_item_pos_ii.enable_cache()
         self.test_item_neg_ii=self.item_factory.get_test_seq_neg_ii()
@@ -167,10 +122,11 @@ class BlockCacheableSequenceSOTests(CacheableSequenceSOTests):
     TestClass=BlockCacheableSequence
 
     def setUp(self):
-        self.item_factory=TestNumberSequenceFactory(self.TestClass)
-        seq_len = TestNumberSequenceFactory.test_len
+        self.item_factory=OrderOfMagnitudeSequenceFactory(
+            self.TestClass)
+        seq_len = OrderOfMagnitudeSequenceFactory.test_len
             # Shortcut: the length of the sequences is equal to the
-            # value defined by TestNumberSequenceFactory.test_len
+            # value defined by OrderOfMagnitudeSequenceFactory.test_len
         self.test_item_pos_ii=self.item_factory.get_test_seq_pos_ii()
         self.test_item_pos_ii.enable_cache()
         self.test_item_pos_ii[0:seq_len:2]

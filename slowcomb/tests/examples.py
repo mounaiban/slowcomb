d1c8226f87078b683153bef2436ce6cb9a204874
Mounaiban <29518031+mounaiban@users.noreply.github.com>
2019-03-30 17:27:28 +1100

    Add module
    The shared specimen module is intended to replace module-specific test data, with the hope that it will streamline the process of troubleshooting issues discovered by automated tests.

---
+++ slowcomb/tests/examples.py
@@ -0,0 +1,46 @@
+
+"""
+slowcomb.tests.examples — Shared Specimens for Manual and Automated Tests
+"""
+
+# Copyright © 2019 Moses Chong
+#
+# This file is part of the Slow Addressable Combinatorics Library (slowcomb)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+import itertools
+from slowcomb.slowcomb import CatCombination, Combination, \
+    CombinationWithRepeats, Permutation, PermutationWithRepeats
+
+
+# Loose Items
+#
+src_colonel = (('I',),('need', 'want'), ('sugar', 'spice', 'scissors'))
+    # A sequence of sequences for use with the CatCombination
+    # combinatorial unit
+
+# Test Data Helper Functions
+#
+def get_latin_upper_alphas(n):
+    """Get the first n uppercase letters of the Latin Alphabet
+    (also called the Roman Alphabet)
+    """
+
+    if n<1 or n>26:
+        raise ValueError('Latin alphabet test seqs are 1-26 letters')
+    return [chr(65+x) for x in range(n+1)]
+

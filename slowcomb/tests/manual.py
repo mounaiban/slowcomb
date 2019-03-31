2b34256ef6c7f3b3ecc7d9dafdb202d7399cc11d
Mounaiban <29518031+mounaiban@users.noreply.github.com>
2019-03-31 21:13:04 +1100

    Rewrite manual test runners
    Rename print_all_terms() to run_as_iter() in Combr and SeqTest
    
    Rewrite CombrTest to print selection data next to each result
    
    Added the ability to select a range of results in CombrTest and SeqTest

--- slowcomb/tests/manual.py
+++ slowcomb/tests/manual.py
@@ -38,61 +38,91 @@ def test_all_docs():
 # Classes
 # 
 class SeqTest:
-
-    def print_all_terms(self):
-        self.seq._i = 0
-        for i in self.seq:
+    
+    def run_as_iter(self):
+        self._seq._i = 0
+        for i in self._seq:
             print(i)
 
-    def run(self):
-        self.print_all_terms()
+    def run(self, i_start=None, i_stop=None):
+        if i_stop is None:
+            if i_start is not None:
+                i_stop=i_start+1
+            else:
+                i_start=0
+                i_stop=len(self._seq)
+
+        count = 0
+        for i in range(i_start, i_stop):
+            self.print_term(i)
+            count += 1
+        print("Showing {0}/{1} terms".format(count, len(self._seq)) )
+
+    def print_term(self, i):
+        print(self._seq[i])
 
     def __repr__(self):
-        seq_repr = "Sequence: {0}".format(repr(self.seq) )
         banner = "Invoke .run() to see all items"
-        return "{0}, {1}".format(seq_repr, banner)
+        name = self.__class__.__name__
+        return "{0}: {1}\n{2}".format(name, repr(self.seq), banner)
 
     def __init__(self, seq):
-        self.seq = seq
+        self._seq = seq
 
 
 class CombrTest(SeqTest):
 
-    def extract_str(self, t):
-        """Extracts strings from an iterable, then combines them into a
-        new single string.
+    def _get_bitmap_str(self, i):
         """
-        sep = ' '
-        out = ""
-        for i in t:
-            if isinstance(i, str):
-                out="".join( (out, sep, str(i)) )
-        return out
+        Get the selection bitmap for the first+i'th combinatioral term,
+        of the combinatorial unit wrapped by this CombrTest, assuming
+        that it uses bitmaps.
 
-    def print_all_addrs(self):
-        for a in range(len(self.seq)):
-            readout = str(self.seq._get_seq_addresses(a))
-            print("Address of {0} is {1}".format(a, readout))
+        """
+        bm = self._seq._bitmap_src[i]
+        if isinstance(bm, int):
+            len_src = len(self._seq._seq_src)
+            return ("{:0"+ str(len_src) +"b}").format(bm)
+        else:
+            raise TypeError('unexpected bitmap format, expected an int')
 
-    def run(self):
-        self.print_all_terms()
-        self.print_all_addrs()
+    def _get_tree_path_str(self, i):
+        """
+        Get the combinatorial tree path for the first+i'th term
+        of the combinatorial unit wrapped by this CombrTest, assuming
+        that it uses trees.
+
+        """
+        tpath = self._seq._get_comb_tree_path(i)
+        return str(tpath)
+
+    def print_term(self, i):
+        # Output combinatorial terms with selection data
+        ii=self._seq._resolve_i(i)
+        out_form = "{0}\t{1}"
+        out=out_form.format(self._selection_data_method(ii), self._seq[i])
+        print(out)
 
     def __repr__(self):
-        seq_repr = "Sequence: {0}".format(repr(self.seq) )
-        banner = "Invoke .run() to see all items and address info"
-        return "{0}, {1}".format(seq_repr, banner)
+        banner = "Invoke .run() to see all items and selection data"
+        name = self.__class__.__name__
+        return "{0}: {1}\n{2}".format(name, repr(self._seq), banner)
 
     def __init__(self, seq):
         super().__init__(seq)
-
+        if(hasattr(self._seq, '_bitmap_src')):
+            self._selection_data_method = self._get_bitmap_str
+        elif(hasattr(self._seq, '_get_comb_tree_path')):
+            self._selection_data_method = self._get_tree_path_str
+        else:
+            raise TypeError('unknown or non-combinatorial unit specified')
 
 # Python Interactive Prompt Welcome Message
 #
 if __name__ != '__main__':
-    print((('hey babe...','Hey, brother!')\
+    print((('hey babe...\n','Hey, brother!\n')\
         [datetime.datetime.now().microsecond%2]))
-    print('This is the Slowcomb Manual Testing Environment')
+    print('Welcome to the Slowcomb Manual Testing Environment')
     print("For a list of ready-to-play test items, try typing")
-    print("'examples' and press the tab key twice.")
+    print("'examples.' (with the dot) and press the tab key twice.")
 

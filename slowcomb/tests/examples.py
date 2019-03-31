331a90478bc0c2616ec393a56b2a80349ddfce88
Mounaiban <29518031+mounaiban@users.noreply.github.com>
2019-03-31 21:03:29 +1100

    Migrate test data to examples module
    Some of the test data have been revised, with more detailed descriptions added

--- slowcomb/tests/examples.py
+++ slowcomb/tests/examples.py
@@ -25,7 +25,10 @@ slowcomb.tests.examples — Shared Specimens for Manual and Automated Tests
 import itertools
 from slowcomb.slowcomb import CatCombination, Combination, \
     CombinationWithRepeats, Permutation, PermutationWithRepeats
-from slowcomb.slowseq import NumberSequence
+from slowcomb.slowseq import NumberSequence, CacheableSequence, \
+    BlockCacheableSequence
+from slowcomb.slowseq import AccumulateSequence, SNOBSequence, SumSequence
+from slowcomb.tests import slowprime
 
 
 # Loose Items
@@ -57,6 +60,22 @@ def get_hindu_arabic_digits(n):
     if n<1 or n>10:
         raise ValueError('Hindu-Arabic numeral test seqs are 1-10 digits')
     return tuple([chr(0x30+x) for x in range(n)])
+
+def get_fruits(n):
+    """Get a tuple of fruit symbols (Emoji) from the Unicode 6.0
+    character set.
+
+    Note that the grocer's definition of fruits is used herein,
+    genetics and taxonomics be damned. This means that the
+    aubergine/brinjal/eggplant and tomato are excluded. Also, the 
+    newer fruit Emoji are not included.
+
+    Special thanks to the contributors at Emojipedia!
+
+    """
+    if n<1 or n>13:
+        raise ValueError('The fruit test seq is 1-13 symbols long')
+    return tuple([chr(0x1f347+x) for x in range(n)])
 
 
 # Test Data Helper Classes
@@ -107,4 +126,96 @@ class OrderOfMagnitudeSequenceFactory:
     def __init__(self, seq_class):
         if issubclass(seq_class, NumberSequence) is True:
             self._seq_class = seq_class
+
+
+# Ready-made Number Sequences
+#
+seq_blk_cache = BlockCacheableSequence(slowprime.fasterer_prime, length=65536)
+seq_blk_cache.enable_cache()
+    # A Block Cacheable Sequence of the first 65,536 lazily-evaluated
+    # prime numbers that caches all previously-requested numbers.
+    # Only single lookups are cached. To cache some numbers, request
+    # them as part of a slice like:
+    #
+    # seq_blk_cache[10000:10032]
+    #
+    # Preferably, request very high prime numbers (e.g. 10000th
+    # onwards). Then, observe the time it takes for single
+    # numbers within the slice to be returned, as opposed to
+    # numbers outside the slice. 
+    #
+seq_dict_cache = CacheableSequence(slowprime.fasterer_prime, length=65536)
+seq_dict_cache.enable_cache()
+    # A Cacheable Sequence of the first 65,536 lazily-evaluated
+    # prime numbers that individually caches previously-requested
+    # numbers. Observe the time it takes for a high prime number
+    # (e.g. the 10000th) to be returned when it is first requested,
+    # and when it is requested again.
+    #
+seq_snob = SNOBSequence(8,4)
+    # A Same Number of Bits (SNOB) sequence which includes all
+    # 8-bit binary numbers with four bits active. The numbers are
+    # ordered from the largest to the smallest.
+    # All outputs from the sequence are in decimal by default.
+    # To see them in binary, try the "{:b}" format with print
+    # or str():
+    #
+    # "{:b}".format(seq_snob[0])
+    #
+seq_sum = SumSequence(lambda x:x**2, length=65536)
+    # A Sum Sequence which includes the first 65,536 stages of
+    # 'the sum of squares to x'.
+seq_accu = AccumulateSequence(lambda x:x+1, lambda x,a:x*a, length=65536)
+    # An Accumulate Sequence which includes the first 65,536
+    # stages of 'the multiple of all numbers until x'. Almost 
+    # equivalent to factorial(n+1), or (n+1)!, where
+    # 0 ≤ n ≤ 65536.
+seq_accu_2 = AccumulateSequence(lambda x:x, lambda x,a:x+(-a),
+    length=4096)
+    # A strange Accumulate Sequence which alternates between
+    # adding the latest term to sum of all previous values, and
+    # subtracting the value of all previous terms from the latest
+    # term. The net effect is a sequence where every consecutive
+    # natural number appears twice. Can you figure this one out?
+
+
+# Ready-made Combinatorial Units
+#
+cc = CatCombination(src_colonel, r=3)
+    # Catenating Combination with a fixed r-size of three words.
+cc_no_r = CatCombination(src_colonel)
+    # Catenating Combination with no defined r-size.
+    # Includes all combinations of length 1 to the longest 
+    # possible length (i.e. 3, see src_colonel above).
+comb = Combination(get_latin_upper_alphas(8), r=4)
+    # Combination: including selections of four Latin alphabets
+    # out of the first 8, (A, B, C, D, E, F, G, H).
+comr = CombinationWithRepeats(get_latin_upper_alphas(4), r=4)
+    # Repeats-Permitted Combination: including selections of
+    # four (or less) Latin alphabets, out of the first 4.
+    # Each alphabet may be selected more than once.
+perm = Permutation(get_latin_upper_alphas(4), r=4)
+    # Permutation: every possible arrangement of the first four
+    # Latin alphabets, (A, B, C, D). This is an example of the
+    # strictest definition of 'permutation'.
+perm_part_no_r = Permutation(get_latin_upper_alphas(4))
+    # Permutation with no defined r-size.
+    # Includes every possible arrangement of any selection of
+    # any number of the first four Latin alphabets.
+perm_part = Permutation(get_latin_upper_alphas(8), r=4)
+    # Permutation: every possible arrangement of a selection of
+    # four Latin alphabets out of the first 8.
+prmr = PermutationWithRepeats(get_hindu_arabic_digits(10), r=5)
+    # Repeats-permitted Permutation: every possible arrangement
+    # of a selection of five Hindu-Arabic digits out of the full
+    # set (0, 1, 2, 3, 4, 5, 6, 7, 8, 9), permitting repetitions
+    # of digits.
+    # This is effectively a counter from 0 to 99999, or the
+    # readouts on a five-dial combination lock.
+prmr_pokies = PermutationWithRepeats(get_fruits(13), r=7)
+    # Repeats-permitted Permuation: every possible arrangement
+    # of a selection of five fruits from a collection of thirteen,
+    # with repetitions allowed.
+    # This is effectively a readout of the payline of a slot
+    # gambling machine.
 

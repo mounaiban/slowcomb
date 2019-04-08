5afab788a1cf667297cd20067c74e7117379a92c
Mounaiban <29518031+mounaiban@users.noreply.github.com>
2019-04-08 13:58:26 +1000

    Remove test_faster_primes(), due to lack of use
    Re-enabled skipped tests for 32,767th prime and smaller.

--- slowcomb/demos/test_slowprime.py
+++ slowcomb/demos/test_slowprime.py
@@ -1,5 +1,5 @@
 """
-test_slowprime — Slow prime number finder self-test module
+Slow prime number finder self-test module
 """
 
 # Copyright © 2019 Moses Chong
@@ -68,20 +68,16 @@ class FasterPrimeOutputTests(unittest.TestCase):
     def test_i_1000(self):
         self.assertEqual(self.func(1000),7919)
 
-    @unittest.skip('Slow test skipped, edit test_slowprime to run')
     def test_i_2000(self):
         # This took ~21s on an AMD A4-7210 in Fedora 29
         self.assertEqual(self.func(2000),17389)
 
-    @unittest.skip('Slow test skipped, edit test_slowprime to run')
     def test_i_5000(self):
         self.assertEqual(self.func(5000),48611)
     
-    @unittest.skip('Slow test skipped, edit test_slowprime to run')
     def test_i_16384(self):
         self.assertEqual(self.func(16384),180503)
 
-    @unittest.skip('Slow test skipped, edit test_slowprime to run')
     def test_i_32767(self):
         self.assertEqual(self.func(32767),386083)
         
@@ -131,35 +127,11 @@ class FastererPrimeOutputTests(FasterPrimeOutputTests):
     """
     func = lambda self,x:fasterer_prime(x)
 
-@unittest.skip('Slow test suite skipped, edit test_slowprime to run')
 class SlowPrimeOutputTests(FasterPrimeOutputTests):
     """Repeat output tests for Slow Primes Finder, slow_prime()"""
     func = lambda self,x:faster_prime(x)
     # SlowPrimeOutputTests were made to be a derivative test of
     # the Faster Prime Tests to make this test case easier to skip
-
-# Functions
-#
-def test_faster_primes(i):
-    # Preliminary test for faster prime number finder functions
-    # Find the i'th prime number
-
-    print("Faster Prime vs Fasterer Prime Number Finders")
-    print("First value is authoritative")
-    stmt_run = "print( '{0}:', {0}({1}) )"
-    stmt_setup = "from tests.slowprime import {0}"
-        # I hope these format templates were easy enough
-        # to understand
-    r_faster=timeit.timeit(stmt_run.format("faster_prime",i),
-        setup=stmt_setup.format("faster_prime"),
-        number=1
-    )
-    r_fasterer=timeit.timeit(stmt_run.format("fasterer_prime",i),
-        setup=stmt_setup.format("fasterer_prime"),
-        number=1
-    )
-    print("{0}s vs {1}s".format(r_faster, r_fasterer))
-
 
 if __name__ == 'main':
     unittest.main()
